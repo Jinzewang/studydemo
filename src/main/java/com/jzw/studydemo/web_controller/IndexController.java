35ba448c793ef0e7805ffa8a66c72fac4fbe8ccd
jinzewang <1948581427@qq.com>
2021-06-21 21:26:31 +0800

    加了一点点

--- src/main/java/com/jzw/studydemo/web_controller/IndexController.java
+++ src/main/java/com/jzw/studydemo/web_controller/IndexController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class IndexController {
     @RequestMapping("/index")
     public String index() {
+        System.out.println("加了一点东西");
         return "index";
     }
 }
